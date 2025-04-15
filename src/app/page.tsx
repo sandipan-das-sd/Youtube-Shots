419c4d9b4a4c8b000206ff06a9fc5823fe0b9e39
sandipan-das-sd <sd901656@gmail.com>
2025-04-15 19:30:34 +0530

    auth added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,37 @@
 import Image from "next/image";
-import {Button} from '@/components/ui/button'
+import { Button } from '@/components/ui/button';
+import { currentUser } from "@clerk/nextjs/server";
+import { prisma } from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+  
+  if (!user) {
+    return null;
+  }
+  
+  // Missing 'user' after prisma
+  const loggedInUser = await prisma.user.findUnique({
+    where: {
+      clerkUserId: user.id
+    }
+  });
+  
+  if (!loggedInUser) {
+    // Missing 'data:' object in user creation
+    await prisma.user.create({
+      data: {
+        name: user.fullName || "Sandipan Das",
+        email: user.emailAddresses[0].emailAddress,
+        clerkUserId: user.id,
+       
+      }
+    });
+  }
+  
   return (
     <>
-
-    <Button> Hello world </Button></>
+      <Button>Hello world</Button>
+    </>
   );
 }
